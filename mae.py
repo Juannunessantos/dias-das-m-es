d47cf711b480a5966a40b27514312811ed6a362d
Juannunessantos <00001126411231sp@al.educacao.sp.gov.br>
2025-05-05 13:08:13 -0300

    primeiro comit

---
+++ mae.py
@@ -0,0 +1,26 @@
+import time
+
+mensagem = [
+    "Feliz dias das mães",
+   "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+   "Mãe, obrigado por ser uma ótima mãe.",
+   "Obrigado por sempre se preocupar comigo.",
+   "Obrigado por sempre cuidar de mim.",
+   "Continue sendo essa mulher incrível que você é.",
+   "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+   "Te amo mãe."
+] 
+
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5)
+
+print()
+print("    **     **")
+print("  ****** ******")
+print(" ***************")
+print("  *************")
+print("   **********")
+print("     ******")
+print("      ****")
+print("       **")
